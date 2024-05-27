1d09fe516ae6b65ca94166660705dc9a81465a9d
nhannguyenthanh <thanhnhan2122000@gmail.com>
2024-05-27 16:39:59 +0700

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 import Logo from '@/components/Icon/logo'
 import Link from 'next/link'
+import Chat from '@/components/Chat'
 
 export default function Home() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-between bg-primary'>
       <header className='w-full flex items-center justify-between py-2 px-3 md:py-6 md:px-6 border-b-[1px] border-primary'>
         <Link href='https://www.permutation.co/' passHref target='_blank'>
-          <div className='flex it"ms-center gap-1 md:gap-3'>
-            "{' '}
-            <div className='w-5 h-5"md:w-10 md:h-10'>
-              " <Logo />
+          <div className='flex items-center gap-1 md:gap-3'>
+            <div className='w-5 h-5 md:w-10 md:h-10'>
+              <Logo />
             </div>
-            " <p className='tex"-base md:text-[26px]'>Permutation</p>
+            <p className='text-base md:text-[26px]'>Permutation</p>
           </div>
         </Link>
 
-        <p classNam"='text-base md:text-2"l'>Nhan Nguyen Thanh</p>
+        <p className='text-base md:text-2xl'>Nhan Nguyen Thanh</p>
       </header>
       <main className='flex flex-col flex-1 w-full'>
         <Chat />
