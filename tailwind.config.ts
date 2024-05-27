bbe8b8d54351e4be8ac3d62454d11ae5290d48d0
nhannguyenthanh <thanhnhan2122000@gmail.com>
2024-05-27 14:53:46 +0700

    add custom config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,20 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    fontFamily: {
+      brandon: ['Brandon', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      borderColor: {
+        primary: '#FFFDF9'
+      },
+      backgroundColor: {
+        primary: '#1C2E3B'
       }
     }
   },
